4196ca42768c48cbe6d8ad4abda5f6a65018992a
Yanxiang Wang <yanxiang.wang@lonelyplanet.com.au>
2017-11-14 08:43:18 +1100

    Add more spec to verify expense data persistence and put the example on pending

--- app/api.rb
+++ app/api.rb
@@ -3,6 +3,10 @@ require 'json'
 
 module ExpenseTracker
   class API < Sinatra::Base
+    get '/expenses/:date' do
+      JSON.generate([])
+    end
+
     post '/expenses' do
       JSON.generate('expense_id' => 42)
     end
--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -7,24 +7,47 @@ module ExpenseTracker
   RSpec.describe 'Expense Tracker API' do
     include Rack::Test::Methods
 
-    def app
-      ExpenseTracker::API.new
-    end
-
-    it 'records submitted expenses' do
-      coffee = {
-        'payee' => 'Market Lane Coffee',
-        'amount' => 4.8,
-        'date' => '2017-06-10'
-      }
-
-      post '/expenses', JSON.generate(coffee)
-
+    def post_expense(expense)
+      post '/expenses', JSON.generate(expense)
       expect(last_response.status).to eq(200)
 
       parsed = JSON.parse(last_response.body)
 
       expect(parsed).to include('expense_id' => a_kind_of(Integer))
+      expense.merge('id' => parsed['expense_id'])
+    end
+
+    def app
+      ExpenseTracker::API.new
+    end
+
+    it 'records submitted expenses' do
+      pending 'Need to persist expenses'
+      coffee = post_expense(
+        'payee' => 'Market Lane Coffee',
+        'amount' => 4.8,
+        'date' => '2017-06-10'
+      )
+
+      commute = post_expense(
+        'payee' => 'PTV',
+        'amount' => 7.8,
+        'date' => '2017-06-10'
+      )
+
+      groceries = post_expense(
+        'payee' => 'Woolie',
+        'amount' => 70.99,
+        'date' => '2017-11-11'
+      )
+
+      get '/expenses/2017-06-10'
+
+      expect(last_response.status).to eq(200)
+
+      expenses = JSON.parse(last_response.body)
+
+      expect(expenses).to contain_exactly(coffee, commute)
     end
   end
 end
