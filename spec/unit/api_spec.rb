319d386f635a48797d1b82daa77af8479f576477
Yanxiang Wang <yanxiang.wang@lonelyplanet.com.au>
2017-11-15 07:57:52 +1100

    Stub storage instance and add implementation for POST expenses

--- spec/unit/api_spec.rb
+++ spec/unit/api_spec.rb
@@ -1,9 +1,33 @@
+require 'rack/test'
 require_relative '../../app/api'
+
 module ExpenseTracker
+  RecordResult = Struct.new(:success?, :expense_id, :error_message)
+
   RSpec.describe API do
+    include Rack::Test::Methods
+
+    def app
+      API.new(ledger: ledger)
+    end
+
+    let(:ledger) { instance_double('ExpenseTracker::Ledger') }
+
     describe 'POST /expenses' do
       context 'when the expense is successfully recorded' do
-        it 'returns the expense id'
+        it 'returns the expense id' do
+          expense = { 'some' => 'data' }
+
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(true, 417, nil))
+
+          post '/expenses', JSON.generate(expense)
+
+          parsed = JSON.parse(last_response.body)
+
+          expect(parsed).to include('expense_id' => 417)
+        end
         it 'responds with a 200 (OK)'
       end
 
