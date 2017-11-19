bade093e74ff450d6bb8661b7d81da2bdbb823b3
Yanxiang Wang <tauruswyx@gmail.com>
2017-11-19 16:41:10 +1100

    Wrap test examples tagged with :db as a rollback DB transaction

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,6 +31,10 @@ RSpec.configure do |config|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
+  config.when_first_matching_example_defined(:db) do
+    require_relative 'support/db'
+  end
+
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
